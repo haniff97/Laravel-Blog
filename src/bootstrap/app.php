6928e139bfba1184725ee50177a13305a8a254cf
haniff97 <hn1ff.97@gmail.com>
2026-05-07 22:27:15 +0800

    fix error 415 and cache problem

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies (VPS / reverse proxy fix for 419 CSRF errors)
+        $middleware->trustProxies(at: '*');
+
         $middleware->append(\Spatie\ResponseCache\Middlewares\CacheResponse::class);
         $middleware->append(\Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class);
     })
--- src/routes/auth.php
+++ src/routes/auth.php
@@ -10,8 +10,9 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use Spatie\ResponseCache\Middlewares\DoNotCacheResponse;
 
-Route::middleware('guest')->group(function () {
+Route::middleware(['guest', DoNotCacheResponse::class])->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
